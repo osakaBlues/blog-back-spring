3cb2a99a28ca06f6ee4a0e3831180c593ca56b40
Sung-il Yoon <books13456@gmail.com>
2023-01-03 16:43:13 +0900

    add cors

---
+++ src/main/java/com/osakablues/blog/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.osakablues.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://osakablues.site",
+                        "https://osakablues.site"
+                )
+                .allowedMethods("GET", "POST")
+                .maxAge(3000);
+    }
+}
